77af605198cf17be408b7826a0dce22504319b99
Joel Abeyta <joel@crane-west.com>
2014-11-17 16:22:06 -0600

    login

--- verify-empl-contact.php
+++ verify-empl-contact.php
@@ -46,8 +46,7 @@
 
 			// email stuff here
 			//define the receiver of the email
-			// $to = 'joel@crane-west.net, BChoate@marketwave.biz';
-			$to = 'joel@crane-west.net';
+			$to = 'joel@crane-west.net, BChoate@marketwave.biz';
 
 			//define the subject of the email
 			$subject = 'crosstexas.com Contact Form submission from '.$_POST['field_5_required']; 
